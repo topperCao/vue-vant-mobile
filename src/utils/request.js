6d493034f66df596ff62d7c4ee88994c21e4351b
@hanxueqiang <hanxueqiang@edspay.com>
2019-03-26 15:57:46 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -20,57 +20,63 @@ const service = axios.create({
  * @params {object} data 参数数据
  * @params {boolean} isSelf 控制是否自行处理接口错误
  */
-service.interceptors.request.use(config => {
-  // 携带token
-  if (store.getters) {
-    config.headers.token = store.getters.token
-  }
-
-  // get方式 data转params
-  if (config.method === 'get') {
-    if (!config.params && config.data) {
-      config.params = config.data
-      config.data = undefined
+service.interceptors.request.use(
+  config => {
+    // 携带token
+    if (store.getters) {
+      config.headers.token = store.getters.token
     }
-  }
 
-  return config
-}, err => {
-  Promise.reject(err)
-})
+    // get方式 data转params
+    if (config.method === 'get') {
+      if (!config.params && config.data) {
+        config.params = config.data
+        config.data = undefined
+      }
+    }
+
+    return config
+  },
+  err => {
+    Promise.reject(err)
+  }
+)
 
 // 响应拦截
-service.interceptors.response.use(response => {
-  const res = response.data
-  // code为1000，正常返回数据
-  if (res.code === 1000) {
-    return res
-  }
-  if (res.code === 20001 || res.code === 1155) {
-    Dialog.alert({
-      title: '提示',
-      message: '登录失效，请重新登录'
-    }).then(() => {
-      window.location.reload()
-    })
-  } else {
-    // isSelf为true，返回数据
-    if (response.config.isSelf) {
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+    // code为1000，正常返回数据
+    if (res.code === 1000) {
       return res
     }
-    // 不符合预期，抛出错误信息
+    if (res.code === 20001 || res.code === 1155) {
+      Dialog.alert({
+        title: '提示',
+        message: '登录失效，请重新登录'
+      }).then(() => {
+        window.location.reload()
+      })
+    } else {
+    // isSelf为true，返回数据
+      if (response.config.isSelf) {
+        return res
+      }
+      // 不符合预期，抛出错误信息
+      Toast({
+        message: 'Error：' + res.msg,
+        duration: 5 * 1000
+      })
+      return Promise.reject(res.msg)
+    }
+  },
+  err => {
     Toast({
-      message: 'Error：' + res.msg,
+      message: 'Error：' + err.message,
       duration: 5 * 1000
     })
-    return Promise.reject(res.msg)
+    return Promise.reject(err)
   }
-}, err => {
-  Toast({
-    message: 'Error：' + err.message,
-    duration: 5 * 1000
-  })
-  return Promise.reject(err)
-})
+)
 
 export default service
