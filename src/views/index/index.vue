7c409abc2eb5d78adc02fc2fce0334348d3e4579
neohan666 <neohan666@aliyun.com>
2019-04-08 10:03:47 +0800

    update

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -4,14 +4,28 @@
 
     <van-button @click="goPage('login')">登录</van-button>
 
+    <div class="test">
+      <button :class="['blue', { radius: flag }]">1</button>
+      <my-child></my-child>
+    </div>
   </div>
 </template>
 
 <script>
 import TopHeader from '@/components/TopHeader/index.vue'
+import MyChild from './MyChild.vue'
 
 export default {
-  components: { TopHeader },
+  components: {
+    TopHeader,
+    MyChild
+  },
+
+  data () {
+    return {
+      flag: true
+    }
+  },
 
   methods: {
     goPage (name) {
@@ -22,5 +36,16 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.test {
+  margin-top: 20px;
+  border: 1px solid #f9f9f9;
+}
 
+.blue {
+  background-color: #409eff;
+  padding: 5px 20px;
+}
+.radius {
+  border-radius: 20px;
+}
 </style>
